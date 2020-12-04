25c409b4f959259e6c9100a1dc06e80feaaadd56
Jakob Jarosch <jakob.jarosch@meisterplan.com>
2020-12-04 21:18:50 +0100

    Configure update to be optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,22 @@
 set -e
 trap '' TERM INT HUP
 
-# Ensure csgo is up to date
-./steamcmd.sh +login anonymous +force_install_dir ./csgo +app_update 740 validate +quit
+if [[ ! -z "$VALIDATE_ON_STARTUP" ]]; then
+    # Ensure csgo is up to date
+    ./steamcmd.sh +login anonymous +force_install_dir ./csgo +app_update 740 validate +quit
+fi
 
 if [ -d /home/steam/htdocs ]; then
-	echo "Copying htdocs..."
-	cp -fR /home/steam/csgo/csgo/maps /home/steam/htdocs
-	cp -fR /home/steam/csgo/csgo/sound /home/steam/htdocs
+    echo "Copying htdocs..."
+    cp -fR /home/steam/csgo/csgo/maps /home/steam/htdocs
+    cp -fR /home/steam/csgo/csgo/sound /home/steam/htdocs
 fi
 
 cd csgo
-./srcds_run -game csgo +exec server.cfg +hostname "$CSGO_HOSTNAME" +sv_password "$CSGO_PASSWORD" +rcon_password "$RCON_PASSWORD" +sv_setsteamaccount "$STEAM_ACCOUNT_TOKEN" +map de_dust2
+./srcds_run -game csgo +exec server.cfg \
+                       +hostname "$CSGO_HOSTNAME" \
+                       +sv_password "$CSGO_PASSWORD" \
+                       +rcon_password "$RCON_PASSWORD" \
+                       -usercon \
+                       +sv_setsteamaccount "$STEAM_ACCOUNT_TOKEN" \
+                       +map de_dust2
